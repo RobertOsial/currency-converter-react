49509893a93d87cf872537d047c24081e45ce93a
Robert Osial <rob.osial@gmail.com>
2021-11-08 23:25:27 +0100

    added Container component with styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import Container from "./Container";
 
 function App() {
   return (
-    <p>Hello!</p>
+    <Container>
+      <form className="form">
+        <h1 className="form__header">Kantor online</h1>
+        <p>
+          <label><span className="form__labelText">Kwota w zł*:</span>
+            <input placeholder="Wpisz kwotę w zł" className="form__field" type="number" required
+              step="0.01" />
+          </label>
+        </p>
+        <p>
+          <label><span className="form__labelText">Waluta:</span>
+            <select className="form__field ">
+              <option value="USD">Dolar amerykański</option>
+              <option value="EUR">Euro</option>
+              <option value="GBP">Funt brytyjski</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <button className="form__button">Przelicz</button>
+        </p>
+        <p className="form__result"></p>
+      </form>
+    </Container>
   );
 }
 
