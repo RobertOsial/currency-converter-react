621c513f7c7ac6949c4d821cb98d19de6437df9c
Robert Osial <rob.osial@gmail.com>
2021-11-12 20:35:24 +0100

    added currencies to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Container from "./Container";
 import Form from "./Form";
 
+const currencies = [
+  {
+    name: "Dolar amerykański",
+    abbreviation: "USD",
+    rate: 4.0559,
+  },
+  {
+    name: "Euro",
+    abbreviation: "EUR",
+    rate: 4.6412,
+  },
+  {
+    name: "Funt brytyjski",
+    abbreviation: "GBP",
+    rate: 5.4328,
+  },
+  {
+    name: "Frank szwajcarski",
+    abbreviation: "CHF",
+    rate: 4.3984,
+  },
+]
+
 function App() {
   return (
     <Container>
-      <Form />
+      <Form currencies={currencies} />
     </Container>
   );
 }
