0a540fa1ba7609202bb9929869cb4b88251361c1
Robert Osial <rob.osial@gmail.com>
2021-11-12 20:38:24 +0100

    added interaction to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,58 @@
 import "./style.css";
-import Header from "../Header";
-import Result from "../Result";
+import Header from "./Header";
+import Result from "./Result";
+import { useState } from "react";
 
-const Form = () => {
+const Form = ({ currencies }) => {
+    const [amount, setAmount] = useState("");
+    const [selectedRate, setselectedRate] = useState(4.0559)
+    const [result, setResult] = useState();
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        calculateResult(amount, selectedRate);
+    };
+
+    const calculateResult = (amount, selectedRate) => {
+        setResult(+amount / selectedRate)
+        console.log(result);
+    };
+
+
     return (
-        <form className="form">
-            <Header title="Kantor online" />
+        <form className="form" onSubmit={onFormSubmit}>
+            <Header title="Kantor Online" />
             <p>
-                <label><span className="form__labelText">Kwota w zł*:</span>
-                    <input placeholder="Wpisz kwotę w zł" className="form__field" type="number" required
+                <label value={amount} onChange={({ target }) => setAmount(target.value)}><span className="form__labelText">Kwota w zł*:</span>
+                    <input
+                        placeholder="Wpisz kwotę w zł"
+                        className="form__field"
+                        type="number"
+                        required
                         step="0.01" />
                 </label>
             </p>
             <p>
                 <label><span className="form__labelText">Waluta:</span>
-                    <select className="form__field ">
-                        <option value="USD">Dolar amerykański</option>
-                        <option value="EUR">Euro</option>
-                        <option value="GBP">Funt brytyjski</option>
+                    <select className="form__field"
+                        value={selectedRate}
+                        onChange={({ target }) => setselectedRate(target.value)}
+                    >
+                        {currencies.map((currency) =>
+                            <option value={currency.rate}>{currency.name}</option>
+                        )}
                     </select>
                 </label>
             </p>
             <p>
                 <button className="form__button">Przelicz</button>
             </p>
-            <Result />
-        </form>);
+            <Result
+                result={result}
+                amount={amount}
+            />
+        </form>
+    );
 }
 
 export default Form;
