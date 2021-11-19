06564d27f8cbf879c7f52e30f1738366f7efa8be
Robert Osial <rob.osial@gmail.com>
2021-11-19 20:30:15 +0100

    added data file json

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        name: "Dolar amerykański",
+        abbreviation: "USD",
+        rate: 4.0559,
+    },
+    {
+        name: "Euro",
+        abbreviation: "EUR",
+        rate: 4.6412,
+    },
+    {
+        name: "Funt brytyjski",
+        abbreviation: "GBP",
+        rate: 5.4328,
+    },
+    {
+        name: "Frank szwajcarski",
+        abbreviation: "CHF",
+        rate: 4.3984,
+    },
+]
