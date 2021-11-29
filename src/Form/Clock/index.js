091dc6053e04d65c5a920a6ebfd2dba4408e1d45
Robert Osial <rob.osial@gmail.com>
2021-11-29 09:07:55 +0100

    added formatDate function

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,31 +2,33 @@ import { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 import "./style.css";
 
+const formatDate = date => date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long"
+})
+
 const Clock = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const internalId = setInterval(() => {
-            setDate(new Date);
-        }, 1000);
+            setDate(new Date())
+        }, 1000)
 
         return () => {
             clearInterval(internalId);
         }
-    }, []);
+    });
 
     return (
         <div className="clock">
             Dzisiaj jest
             {" "}
-            {date.toLocaleString(undefined, {
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                day: "numeric",
-                month: "long"
-            })}
+            {formatDate(date)}
         </div>
     );
 }
