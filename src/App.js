32213c8133a2a686c6d5669d8faa916b103da90a
Robert Osial <rob.osial@gmail.com>
2021-12-17 09:46:55 +0100

    axios get request added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Form from "./Form";
 import Clock from "./Clock";
 import { Wrapper } from "./styled";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { currencies } from "./currencies";
+import axios from "axios";
 
 function App() {
   const [result, setResult] = useState("")
+  const apiURL = "https://api.exchangerate.host/latest";
+
+
+  const fetchData = async () => {
+    const response = await axios.get(apiURL);
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   const calculateResult = (amount, currency) => {
     const rate = currencies
