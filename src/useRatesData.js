611f5ab7129bbb0d4ed5f2781bfd9992e3c5b064
Robert Osial <rob.osial@gmail.com>
2021-12-30 16:01:25 +0000

    created useRatesData hook

---
+++ src/useRatesData.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({
+        status: "loading",
+    });
+    const apiURL = "https://api.exchangerate.host/latest?base=PLN";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(apiURL);
+                const { rates, date, base: baseCurrency } = response.data;
+
+                setRatesData({
+                    status: "success",
+                    rates,
+                    date,
+                    baseCurrency,
+                })
+
+            } catch {
+                setRatesData({
+                    status: "error",
+                })
+            }
+        };
+        setTimeout(fetchData, 1000);
+
+    }, []);
+
+    return ratesData;
+};
+
+
+
+
+
+
+
+
+
