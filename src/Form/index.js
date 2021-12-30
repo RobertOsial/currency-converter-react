eee38389f57d0583096b074883fac7eacb05a4e0
Robert Osial <rob.osial@gmail.com>
2021-12-30 16:04:28 +0000

    adjustment Form Component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,17 +1,32 @@
 import Header from "./Header";
 import Result from "./Result";
+import Info from "./Info";
 import {
     LabelText,
     Field,
-    Button
+    Button,
+    Loading,
+    Failure,
 } from "./styled";
 
-import { currencies } from "../currencies";
 import { useState } from "react";
+import { useRatesData } from "../useRatesData";
 
-const Form = ({ result, calculateResult }) => {
+const Form = () => {
+    const [result, setResult] = useState();
     const [amount, setAmount] = useState("");
-    const [currency, setCurrency] = useState(currencies[0].short);
+    const [currency, setCurrency] = useState("EUR");
+    const { rates, status, date, baseCurrency } = useRatesData();
+
+    const calculateResult = (amount, currency) => {
+        const rate = rates[currency]
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount * rate,
+            currency,
+        });
+    }
 
     const onFormSubmit = (e) => {
         e.preventDefault();
@@ -21,40 +36,56 @@ const Form = ({ result, calculateResult }) => {
     return (
         <form onSubmit={onFormSubmit} >
             <Header title="Kantor Online" />
-            <p>
-                <label><LabelText>Kwota w zł*:</LabelText>
-                    <Field
-                        placeholder="Wpisz kwotę w zł"
-                        type="number"
-                        required
-                        step="0.01"
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <label><LabelText>Waluta:</LabelText>
-                    <Field
-                        as="select"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency) => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        ))}
-                    </Field>
-                </label>
-            </p>
-            <p>
-                <Button>Przelicz</Button>
-            </p>
-            <Result result={result} />
+            {status === "loading" ? (
+                <Loading>
+                    Ładuję dane... Poczekaj chwilę...
+                </Loading>
+            ) : (
+                status === "error" ? (
+                    <Failure>
+                        Coś poszło nie tak... Spróbuj ponownie później...
+                    </Failure>
+                ) : (
+                    <>
+                        <p>
+                            <label><LabelText>Kwota w zł*:</LabelText>
+                                <Field
+                                    placeholder="Wpisz kwotę w zł"
+                                    type="number"
+                                    required
+                                    step="0.01"
+                                    value={amount}
+                                    onChange={({ target }) => setAmount(target.value)}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <label><LabelText>Waluta:</LabelText>
+                                <Field
+                                    as="select"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
+                                >
+                                    {Object.keys(rates).map((currency) => (
+                                        <option
+                                            key={currency}
+                                            value={currency}
+                                        >
+                                            {currency}
+                                        </option>
+                                    ))}
+                                </Field>
+                            </label>
+                        </p>
+                        <p>
+                            <Button>Przelicz</Button>
+                        </p>
+                        <Result result={result} baseCurrency={baseCurrency} />
+                        <Info date={date} />
+                    </>
+                )
+            )
+            }
         </form>
     );
 }
