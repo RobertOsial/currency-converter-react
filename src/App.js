15b131aa17584db8e32b04d7e6bfe3621f753568
Robert Osial <rob.osial@gmail.com>
2021-12-30 16:02:18 +0000

    moved form functionality to Form Component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import Form from "./Form";
 import Clock from "./Clock";
 import { Wrapper } from "./styled";
-import { useEffect, useState } from "react";
-import { currencies } from "./currencies";
-import axios from "axios";
 
 function App() {
-  const [result, setResult] = useState("")
-  const apiURL = "https://api.exchangerate.host/latest";
 
-
-  const fetchData = async () => {
-    const response = await axios.get(apiURL);
-    console.log(response.data);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  const calculateResult = (amount, currency) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  }
-
-  return (
-    <Wrapper>
-      <Clock />
-      <Form result={result} calculateResult={calculateResult} />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Clock />
+            <Form />
+        </Wrapper>
+    );
 }
 
 export default App;
